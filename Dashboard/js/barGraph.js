f788b048bc8c232ef4485c0b4314a01f913f06d8
Oliver Mansfield <o.mansfield@amaze.com>
2015-12-08 10:34:23 +0000

    starting to animate bar graph
    trying to figure out how to animate the bar graph

--- Dashboard/js/barGraph.js
+++ Dashboard/js/barGraph.js
@@ -29,25 +29,18 @@ function createBar(){
 
         //Create xScale to use with horizontal sizing.
         //Have to use zScoresAllPositive otherwise the graph is off.
-        // -19 could be the longest bar, but 8 would still be larger so would be considered the 'max'.
+        // -19 could be the longest bar, but 8 would still be larger so would be considered the 'max'
+        // even though -19 should be the longest bar.
     	var xScale = d3.scale.linear(data)
                     .domain([0, d3.max(zScoresAllPositive)])
                     .range([0, frameWidth]);
 
-        //DRAWING CODE.
-        //
+        //DRAWING CODE.        
 	    var bars = frame.selectAll('rect')
 	    				.data(data)
 	    				.enter()
 	    					.append('rect')
-	    					.attr('width', function(d, i){
-	    						if (zScoresArray[i] < 0){  						
-	    							return xScale(Math.abs(zScoresArray[i])) /2;
-	    						}
-	    						else {
-	    							return xScale(zScoresArray[i]) /2;
-	    						}
-	    					})
+	    					.attr('width', 0)
 	    					.attr('height', 20)
 	    					.attr('x' , function (d, i){
 	    						if (zScoresArray[i] < 0) {
@@ -63,6 +56,21 @@ function createBar(){
 							.attr('fill', function(d, i){
                                 return colours[i];
                             })
+
+
+        //Animate
+      bars.transition()
+          .duration(1000)    
+		       .attr('width', function(d, i){
+					if (zScoresArray[i] < 0){  						
+						return xScale(Math.abs(zScoresArray[i])) /2;
+					}
+					else {
+						return xScale(zScoresArray[i]) /2;
+					}
+				})
+           //moving the bars from the bottom of the frame, minus the height of the bar.
+           //Which means it'll sit at the bottom of the graph.
 
 	});
 
